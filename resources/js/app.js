a2df68bf3f5d83b6b88564883bb9b01f1e4848ae
Jean Carlo <jeanca2198@gmail.com>
2020-05-15 11:34:32 -0500

    switch bootstrap-vue+adminlte to vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,15 +2,20 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+// LOAD VUETIFY
+import Vuetify from 'vuetify'
+Vue.use(Vuetify);
+// import BootstrapVue from 'bootstrap-vue' //Importing
 
-import BootstrapVue from 'bootstrap-vue' //Importing
+// Vue.use(BootstrapVue) // Telling Vue to use this in whole application
 
-Vue.use(BootstrapVue) // Telling Vue to use this in whole application
-
-
+//REGISTRO DE COMPONENTES
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('navbar', require('./components/NavBar.vue').default);
 
 
 const app = new Vue({
     el: '#app',
+    //añade la instancia de Vuetify como parámetro en la de Vue
+    vuetify: new Vuetify(),
 });
