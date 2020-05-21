58e8c6eea9d6a4582a1fd748231bf1a2c2b8f801
Jean Carlo <jeanca2198@gmail.com>
2020-05-21 12:30:16 -0500

    cambio texto bienvenida

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,7 +56,7 @@
                 <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="#69F0AE" dark>
                     <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
                     <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
-                        <span class="hidden-sm-and-down">{{auth()->user()->name}}</span>
+                        <span style='color : #9F9E9D' class="hidden-sm-and-down">Bienvenido, {{auth()->user()->name}}</span>
                     </v-toolbar-title>
                     <v-text-field flat solo-inverted hide-details prepend-inner-icon="mdi-magnify" label="Buscar producto"
                         class="hidden-sm-and-down"></v-text-field>
