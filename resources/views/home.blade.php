ca6eb6053765e15cc79c2de9f8a541e6a10d071e
Jean Carlo <jeanca2198@gmail.com>
2020-05-21 19:45:36 -0500

    sprint 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 @section('content')
     @if(@Auth::user()->hasRole('consumidor'))
-
+    <home-consumidor></home-consumidor>
     @elseif(@Auth::user()->hasRole('comerciante'))
-    @elseif(@Auth::user()->hasRole('administrador'))
-    <consumidores></consumidores> 
+    <home-comerciante></home-comerciante>
 
+    @elseif(@Auth::user()->hasRole('administrador'))
+        <consumidores></consumidores>
     @endif
 @endsection
