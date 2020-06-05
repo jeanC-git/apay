0c0a9b9650a7ca1a8080d8bd4acb0ca24d0771e4
yoliza <1628984@utp.edu.pe>
2020-06-05 11:39:04 -0500

    lol

--- resources/js/components/Consumidor/InfoPersonal.vue
+++ resources/js/components/Consumidor/InfoPersonal.vue
@@ -1,4 +1,28 @@
 <template>
-<h1>HOLA</h1>
+  <v-app id="inspire">
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
 
+        >
+        <v-row
+          align="center"
+          justify="center"
+
+        >
+          <v-col class="mt-0"
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12"
+                     color="blue">
+           
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
