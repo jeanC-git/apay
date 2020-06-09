71ef7d96e0f73b494f84dc5c5c97864a5bfac76d
Jean Carlo <jeanca2198@gmail.com>
2020-06-09 00:09:25 -0500

    cambios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,9 +55,9 @@
                     <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
                         <span style='color : #9F9E9D' class="hidden-sm-and-down">Bienvenido, {{auth()->user()->name}}</span>
                     </v-toolbar-title>
-                    <v-text-field flat solo-inverted hide-details prepend-inner-icon="mdi-magnify" label="Buscar producto"
+                    {{-- <v-text-field flat solo-inverted hide-details prepend-inner-icon="mdi-magnify" label="Buscar producto"
                         class="hidden-sm-and-down">
-                    </v-text-field>
+                    </v-text-field> --}}
                     <v-spacer></v-spacer>
                     <v-btn icon>
                         <v-icon>mdi-bell</v-icon>
@@ -76,6 +76,6 @@
             </v-app>
         </template>
     </v-app>
-    
+
 </body>
 </html>
