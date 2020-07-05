50c7a38f06a15647e3cd3c2e87990d2f79247d38
aldo1919 <aldolopezcarrion@gmail.com>
2020-07-05 15:22:46 -0500

    subida

---
+++ migrations/2020_06_04_055521_create_comerciante_productos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComercianteProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comerciante_productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('stock'); 
+            $table->bigInteger('id_comerciante')->unsigned();
+            $table->bigInteger('id_puesto')->unsigned();
+            $table->bigInteger('id_producto')->unsigned();
+            $table->timestamps();
+            $table->foreign('id_comerciante')->references('id')->on('comerciantes'); 
+            $table->foreign('id_producto')->references('id')->on('productos');
+            $table->foreign('id_puesto')->references('id')->on('puestos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comerciante_productos');
+    }
+}
---
+++ seeds/PostTableSeeder.php
@@ -0,0 +1,96 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            ['name' => 'administrador','guard_name' =>'web'],
+            ['name' => 'consumidor','guard_name' =>'web'],
+            ['name' => 'comerciante','guard_name' =>'web'],
+        ]);
+
+        $faker= Faker::create();
+        foreach (range(1,10) as $index) {
+            $user = User::create([
+                'name' => $faker->firstname,
+                'lastname'=>$faker->lastName,
+                'dni' => '123456780',
+                'password'=>Hash::make('12345678'),
+                'email' => $faker->unique()->freeEmail,
+                'dni'   => rand ( 1000000, 9999999 )
+            ]);
+            DB::table('consumidores')->insert([
+                'id_user' => $user->id,
+            ]);
+            $user->assignRole('consumidor');
+        }
+        foreach (range(1,10) as $index) {
+            $user = User::create([
+                'name' => $faker->firstname,
+                'lastname'=>$faker->lastName,
+                'dni' => '123456780',
+
+                'password'=>Hash::make('12345678'),
+                'email' => $faker->unique()->freeEmail,
+                'dni'   =>rand ( 1000000, 9999999 )
+            ]);
+            DB::table('comerciantes')->insert([
+                'id_user' => $user->id,
+            ]);
+            $user->assignRole('comerciante');
+        }
+
+        // USUARIO ADMINISTRADOR
+        $user = User::create([
+            'name' => $faker->firstname,
+            'lastname'=>$faker->lastName,
+            'dni' => '123456780',
+
+            'password'=>Hash::make('12345678'),
+            'email' => 'admin@admin.com',
+            'dni'   =>rand ( 1000000, 9999999 )
+        ]);
+        $user->assignRole('administrador');
+
+        // USUARIO COMERCIANTE
+        $user_comerciante = User::create([
+            'name' => $faker->firstname,
+            'lastname'=>$faker->lastName,
+            'dni' => '123456780',
+
+            'password'=>Hash::make('12345678'),
+            'email' => 'comerciante@comerciante.com',
+            'dni'   =>rand ( 1000000, 9999999 )
+        ]);
+        DB::table('comerciantes')->insert([
+            'id_user' => $user_comerciante->id,
+        ]);
+        $user_comerciante->assignRole('comerciante');
+
+        // USUARIO CONSUMIDOR
+        $user_consumidor = User::create([
+            'name' => $faker->firstname,
+            'lastname'=>$faker->lastName,
+            'dni' => '123456780',
+
+            'password'=>Hash::make('12345678'),
+            'email' => 'consumidor@consumidor.com',
+            'dni'   =>rand ( 1000000, 9999999 )
+        ]);
+        DB::table('consumidores')->insert([
+            'id_user' => $user_consumidor->id,
+        ]);
+        $user_consumidor->assignRole('consumidor');
+    }
+}
